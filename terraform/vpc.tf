0c1f675fa3db8b4aca1e4b72b71a1515462cb419
Joshna7 <81368069+Joshna7@users.noreply.github.com>
2022-09-06 20:37:36 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,47 +1,34 @@
 ### Network Block ###
 ## Define our VPC
-resource "aws_vpc" "elbvpc" {
-  cidr_block = var.vpc_cidr
+resource "aws_vpc" "main" {
+  cidr_block = "172.17.0.0/16"
   enable_dns_hostnames = true
-
-  tags = {
-    Name = "elbvpc"
-  }
 }
+
 ## Define the public subnet
-resource "aws_subnet" "public-subnet" {
-  vpc_id = aws_vpc.elbvpc.id
-  cidr_block = var.public_subnet_cidr
-  availability_zone = "us-west-2a"
-
-  tags = {
-    Name = "lb_Public_Subnet"
-  }
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "172.17.1.0/24"
+  availability_zone = "ap-south-1a"
 }
+
 ## Define the internet gateway
-resource "aws_internet_gateway" "lbgw" {
-  vpc_id = aws_vpc.elbvpc.id
-
-  tags = {
-    Name = "VPC_LB_IGW"
-  }
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
 }
+
 ## Define the route table
 resource "aws_route_table" "public-rt" {
-  vpc_id = aws_vpc.elbvpc.id
+  vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.lbgw.id
-  }
-
-  tags = {
-    Name = "Public_Subnet_RT"
+    gateway_id = aws_internet_gateway.gw.id
   }
 }
 
 ## Assign the route table to the public Subnet
 resource "aws_route_table_association" "public-rt" {
-  subnet_id = aws_subnet.public-subnet.id
+  subnet_id = aws_subnet.public.id
   route_table_id = aws_route_table.public-rt.id
 }
